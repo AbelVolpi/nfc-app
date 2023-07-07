f033076c5b72672359087e15cf9f848f6206b399
AbelVolpi <abelvolpi@gmail.com>
2023-07-07 19:05:01 -0300

    feat: receive-screen layout

--- app/src/main/java/com/abelvolpi/nfc_app/ui/screens/ReceiveScreen.kt
+++ app/src/main/java/com/abelvolpi/nfc_app/ui/screens/ReceiveScreen.kt
@@ -1,18 +1,49 @@
 package com.abelvolpi.nfc_app.ui.screens
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterHorizontally
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.abelvolpi.nfc_app.R
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.rememberLottieComposition
 
 @Composable
 fun ReceiveScreen() {
-    Box(modifier = Modifier.fillMaxSize()) {
-        Text(text = "ReceiveScreen", modifier = Modifier.align(Alignment.Center))
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(modifier = Modifier.padding(bottom = 100.dp)) {
+            LottieAnimation()
+            Spacer(modifier = Modifier.size(20.dp))
+            Text(text = "Searching device", modifier = Modifier.align(CenterHorizontally))
+        }
     }
+}
+
+@Composable
+fun LottieAnimation() {
+    val composition by rememberLottieComposition(
+        spec = LottieCompositionSpec.RawRes(R.raw.nfc_lottie)
+    )
+    LottieAnimation(
+        composition = composition,
+        iterations = LottieConstants.IterateForever
+    )
+}
+
+@Preview
+@Composable
+fun LottieAnimationPreview() {
+    LottieAnimation()
 }
 
 @Preview(showBackground = true)
